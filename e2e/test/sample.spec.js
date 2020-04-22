4c12c69968582a6a0d0238782a4d0fa3bcc09fe5
QixianX <1037916520@qq.com>
2020-04-22 17:57:11 +0800

    shuru

--- e2e/test/sample.spec.js
+++ e2e/test/sample.spec.js
@@ -24,3 +24,26 @@ describe('add task', function () {
         await page.close();
 
     });
+
+
+    //#################输入测试
+
+    it('should add new todo item', async function() {
+
+
+        await page.click('#myInput', {delay: 500});
+
+        await page.type('#myInput', newTaskContent, {delay: 50});
+
+        await page.click('#addButton', {delay: 50});
+
+
+        let todoList = await page.waitFor('#myUL');
+
+        const expectInputContent = await page.evaluate(todoList => todoList.lastChild.querySelector('#myUL #myUL:li').textContent, todoList);
+
+        expect(expectInputContent).to.eql(newTaskContent);
+
+    })
+
+});
