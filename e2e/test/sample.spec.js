9b1737fceeed476d3d6612a1c707d6e5e986a847
QixianX <1037916520@qq.com>
2020-04-22 18:00:43 +0800

    qita

--- e2e/test/sample.spec.js
+++ e2e/test/sample.spec.js
@@ -47,3 +47,95 @@ describe('add task', function () {
     })
 
 });
+
+
+describe('update task', function () {
+
+    let page;
+
+
+
+    before (async function () {
+
+        page = await browser.newPage();
+
+        await page.goto('http://127.0.0.1:80/');
+
+    });
+
+
+
+    after (async function () {
+
+        await page.close();
+
+    });
+
+    
+
+    it('should update task', async function() {
+
+        const updatedContent = 'updated content';
+
+        await page.waitFor('#myUL');
+
+        await page.click('#myUL #myUL:li:last-child');
+
+        const textareaElement=await page.$('.updatetask');
+
+        await textareaElement.click('#submitchange')
+
+        await page.$eval('#myUL #myUL:li:last-child textarea', textarea => textarea.blur());
+
+        let theLastItem = await page.waitFor('#myUL #myUL:li:last-child');
+
+        const expectInputContent = await page.evaluate(task => task.querySelector('textarea').textContent, theLastItem);
+
+        expect(expectInputContent).to.eql(updatedContent);
+
+    });
+
+});
+
+//############### 删除测试
+
+describe('delete task', function () {
+
+    let page;
+
+
+    before (async function () {
+
+        page = await browser.newPage();
+
+        await page.goto('http://127.0.0.1:80/');
+
+    });
+
+
+
+    after (async function () {
+
+        await page.close();
+
+    });
+
+
+
+    it('should delete the new item ', async function() {
+
+        await page.waitFor('#myUL');
+
+        let originalItemsCount = await page.$$('#myUL #myUL:li').then(item => item.length);
+
+        await page.click('#myUL #myUL:li:last-child .close');
+
+        await page.waitFor(500);
+
+        let itemsCount = await page.$$('#myUL #myUL:li').then(item => item.length);
+
+        expect(originalItemsCount - itemsCount).to.eql(1);
+
+    });
+
+});
