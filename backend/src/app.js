84bd331fd2ef202480dcf7753454fafb3e086360
zhudi0730 <50724375+zhudi0730@users.noreply.github.com>
2020-04-22 18:17:47 +0800

    MVC_first_commit

---
+++ backend/src/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser')  //解析前端传输的数据
+const {
+  getAllTodo,
+  deleteTodo,
+  addTodo,
+  updateTodo,
+  deleteTodo2,
+  updateTodo2
+} = require('./controller')
+ 
+const app = express()
+app.locals.dataFilePath = "../data/Tasks.json"
+const port = 3000
+
+app.use(bodyParser.json())  
+app.use(bodyParser.urlencoded({ extended: false})) 
+app.use(express.json())
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,PATCH,OPTIONS");
+  next();
+});
+
+app.post("/api/add", addTodo)
+
+app.get("/api/getAll", getAllTodo)
+
+app.delete("/api/delete", deleteTodo) 
+
+app.post("/api/update", updateTodo)
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+exports.app = app
+
